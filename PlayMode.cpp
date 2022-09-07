903e4b24f2e8d0b7a0c77922a5a29976c7fe9d6a
Weisheng Li <li2546014173@hotmail.com>
2022-09-07 18:05:39 -0400

    set up the background with hard-coded map

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -48,60 +48,78 @@ PlayMode::PlayMode() {
 		}
 	}
 
-	//use sprite 32 as a "player":
-	ppu.tile_table[32].bit0 = {
-		0b01111110,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b01111110,
-	};
-	ppu.tile_table[32].bit1 = {
-		0b00000000,
-		0b00000000,
-		0b00011000,
-		0b00100100,
-		0b00000000,
-		0b00100100,
-		0b00000000,
-		0b00000000,
+	// ignore everything above
+	load_tiles("tiles.asset");
+
+	// set up darkness
+	tile_table[3].bit0 = { 0,0,0,0,0,0,0,0 };
+	tile_table[3].bit1 = { 0,0,0,0,0,0,0,0 };
+	palette_table[3] = {
+		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+		glm::u8vec4(0x00, 0x00, 0x00, 0xff)
 	};
 
-	//makes the outside of tiles 0-16 solid:
-	ppu.palette_table[0] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+	//ppu.palette_table[7] = palette_table[0];
+	//ppu.tile_table[32] = tile_table[0];
+	ppu.palette_table = palette_table;
+	ppu.tile_table = tile_table;
+
+	// map editor
+	std::array<bool, PPU466::BackgroundWidth * PPU466::BackgroundHeight> map = {
+		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+		0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 	};
 
-	//makes the center of tiles 0-16 solid:
-	ppu.palette_table[1] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	};
-
-	//used for the player:
-	ppu.palette_table[7] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0xff, 0xff, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	};
-
-	//used for the misc other sprites:
-	ppu.palette_table[6] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x88, 0x88, 0xff, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-	};
-
+	uint16_t wall_tile = 0x0202;
+	uint16_t floor_tile = 0x0101;
+	uint16_t darkness = 0x0303;
+	std::cout << std::bitset<16>(darkness) << std::endl;
+	uint32_t height = PPU466::BackgroundHeight;
+	uint32_t width = PPU466::BackgroundWidth;
+	for (uint32_t y = 0; y < height; ++y) {
+		for (uint32_t x = 0; x < width; ++x) {
+			if (y >= height / 2 ||
+				x >= width / 2) {
+				
+				ppu.background[x + width * y] = darkness;
+			}
+			else {
+				ppu.background[x + width * y] =
+					map[x + (width / 2) * (height / 2 - 1 - y)] ?
+						wall_tile : floor_tile;
+			}
+		}
+	}
 }
 
 PlayMode::~PlayMode() {
@@ -148,11 +166,6 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 
 void PlayMode::update(float elapsed) {
 
-	//slowly rotates through [0,1):
-	// (will be used to set background color)
-	background_fade += elapsed / 10.0f;
-	background_fade -= std::floor(background_fade);
-
 	constexpr float PlayerSpeed = 30.0f;
 	if (left.pressed) player_at.x -= PlayerSpeed * elapsed;
 	if (right.pressed) player_at.x += PlayerSpeed * elapsed;
@@ -169,43 +182,93 @@ void PlayMode::update(float elapsed) {
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
 	//--- set ppu state based on game state ---
 
-	//background color will be some hsv-like fade:
-	ppu.background_color = glm::u8vec4(
-		std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 0.0f / 3.0f) ) ) ))),
-		std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 1.0f / 3.0f) ) ) ))),
-		std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 2.0f / 3.0f) ) ) ))),
-		0xff
-	);
+	// just regular all white background:
+	ppu.background_color = glm::u8vec4(0xff, 0xff, 0xff, 0xff);
 
 	//tilemap gets recomputed every frame as some weird plasma thing:
 	//NOTE: don't do this in your game! actually make a map or something :-)
-	for (uint32_t y = 0; y < PPU466::BackgroundHeight; ++y) {
-		for (uint32_t x = 0; x < PPU466::BackgroundWidth; ++x) {
-			//TODO: make weird plasma thing
-			ppu.background[x+PPU466::BackgroundWidth*y] = ((x+y)%16);
-		}
-	}
+	//for (uint32_t y = 0; y < PPU466::BackgroundHeight; ++y) {
+	//	for (uint32_t x = 0; x < PPU466::BackgroundWidth; ++x) {
+	//		//TODO: load the map
+	//		ppu.background[x + PPU466::BackgroundWidth * y] = ((x + y) % 16);
+	//	}
+	//}
 
 	//background scroll:
-	ppu.background_position.x = int32_t(-0.5f * player_at.x);
-	ppu.background_position.y = int32_t(-0.5f * player_at.y);
+	//ppu.background_position.x = int32_t(-0.5f * player_at.x);
+	//ppu.background_position.y = int32_t(-0.5f * player_at.y);
+	glm::ivec2 background_position = glm::ivec2(0, 0);
 
 	//player sprite:
 	ppu.sprites[0].x = int8_t(player_at.x);
 	ppu.sprites[0].y = int8_t(player_at.y);
-	ppu.sprites[0].index = 32;
-	ppu.sprites[0].attributes = 7;
-
-	//some other misc sprites:
-	for (uint32_t i = 1; i < 63; ++i) {
-		float amt = (i + 2.0f * background_fade) / 62.0f;
-		ppu.sprites[i].x = int8_t(0.5f * PPU466::ScreenWidth + std::cos( 2.0f * M_PI * amt * 5.0f + 0.01f * player_at.x) * 0.4f * PPU466::ScreenWidth);
-		ppu.sprites[i].y = int8_t(0.5f * PPU466::ScreenHeight + std::sin( 2.0f * M_PI * amt * 3.0f + 0.01f * player_at.y) * 0.4f * PPU466::ScreenWidth);
-		ppu.sprites[i].index = 32;
-		ppu.sprites[i].attributes = 6;
-		if (i % 2) ppu.sprites[i].attributes |= 0x80; //'behind' bit
-	}
+	//ppu.sprites[0].index = 32;
+	//ppu.sprites[0].attributes = 7;
+	ppu.sprites[0].index = 0;
+	ppu.sprites[0].attributes = 0;
 
 	//--- actually draw ---
 	ppu.draw(drawable_size);
 }
+
+void PlayMode::load_tiles(std::string tile_path) {
+	std::ifstream path(tile_path);
+
+	/* read the tiles */
+	unsigned int tile_count;
+	path >> tile_count;
+	// 8 * 8 is the tile size
+	for (uint8_t tile_idx = 0; tile_idx < tile_count; tile_idx++) {
+		PPU466::Tile new_tile{
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0}
+		};
+
+		for (uint8_t i = 0; i < 8 * 8; i++) {
+			unsigned int color_idx_32;
+			path >> color_idx_32;
+			uint8_t color_idx = static_cast<uint8_t> (color_idx_32);
+			assert(color_idx < 4);
+			uint8_t bit0 = color_idx & 1;
+			uint8_t bit1 = (color_idx >> 1) & 1;
+			// std::cout << color_idx_32 << " bit1:" << std::bitset<8>(bit1) << " bit0:" << std::bitset<8>(bit0) << std::endl;
+			unsigned int x = i % 8, y = i / 8;
+
+			new_tile.bit0[y] |= bit0 << (7 - x);
+			new_tile.bit1[y] |= bit1 << (7 - x);
+		}
+
+		tile_table[tile_idx] = new_tile;
+	}
+
+	/* read the tile-pelatte mapping */
+	std::unordered_map <unsigned int, unsigned int> tp_map = {};
+	for (uint8_t tp_map_idx = 0; tp_map_idx < tile_count; tp_map_idx++) {
+		unsigned int tile_idx, palette_idx;
+		path >> tile_idx >> palette_idx;
+		assert(tile_idx < 16 * 16 && palette_idx < 8);
+
+		tp_map[tile_idx] = palette_idx;
+	}
+
+	/* read the palette */
+	unsigned int palette_count;
+	path >> palette_count;
+	for (uint8_t palette_idx = 0; palette_idx < palette_count; palette_idx++) {
+		PPU466::Palette new_palette;
+		for (int i = 0; i < 4; i++) {
+			unsigned int R_32, G_32, B_32, A_32;
+			path >> R_32 >> G_32 >> B_32 >> A_32;
+			// std::cout << R_32 << G_32 << B_32 << A_32 << std::endl;
+			uint8_t R = static_cast<uint8_t> (R_32);
+			uint8_t G = static_cast<uint8_t> (G_32);
+			uint8_t B = static_cast<uint8_t> (B_32);
+			uint8_t A = static_cast<uint8_t> (A_32);
+			glm::u8vec4 RGBA(R, G, B, A);
+
+			new_palette[i] = RGBA;
+		}
+		palette_table[palette_idx] = new_palette;
+	}
+}
+
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -4,7 +4,12 @@
 #include <glm/glm.hpp>
 
 #include <vector>
+#include <string>
 #include <deque>
+#include <cassert>
+#include <bitset>
+#include <unordered_map>
+#include <fstream>
 
 struct PlayMode : Mode {
 	PlayMode();
@@ -15,6 +20,9 @@ struct PlayMode : Mode {
 	virtual void update(float elapsed) override;
 	virtual void draw(glm::uvec2 const &drawable_size) override;
 
+	// asset loading functions
+	void load_tiles(std::string tile_path);
+
 	//----- game state -----
 
 	//input tracking:
@@ -23,13 +31,14 @@ struct PlayMode : Mode {
 		uint8_t pressed = 0;
 	} left, right, down, up;
 
-	//some weird background animation:
-	float background_fade = 0.0f;
-
 	//player position:
 	glm::vec2 player_at = glm::vec2(0.0f);
 
 	//----- drawing handled by PPU466 -----
+	// local storage for ppu assets
+	std::array< uint16_t, PPU466::BackgroundWidth* PPU466::BackgroundHeight > background;
+	std::array< PPU466::Tile, 16 * 16 > tile_table;
+	std::array< PPU466::Palette, 8 > palette_table;
 
 	PPU466 ppu;
 };
